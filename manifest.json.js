7f17096e6ec173416d5418eb0cb4f53370f72e5f
jite7405-sudo <jite7405@gmail.com>
2026-04-08 21:24:50 +0530

    Add files via upload

---
+++ manifest.json.js
@@ -0,0 +1,9 @@
+{
+  "name": "My Pro Tracker",
+  "short_name": "Tracker",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#0a84ff",
+  "icons":
+}
